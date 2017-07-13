9c536e45c1a65e2b57e4cd6ed2e4f412bb8f373f
Kevin Rodriguez <kevin.1296@hotmail.com>
2017-07-12 21:39:53 -0600

    Importantes configuraciones en cuanto al mapeo de la base de dato en el Modelo del Poryecto. Ademas de creacion del primer controlador para los usuarios

---
+++ proyectoWebII_empresa_asp.net/proyectoWebII_empresa_asp.net/Models/proyectoWebII_empresa_aspnetContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace proyectoWebII_empresa_asp.net.Models
+{
+    public class proyectoWebII_empresa_aspnetContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public proyectoWebII_empresa_aspnetContext() : base("name=proyectoWebII_empresa_aspnetContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<proyectoWebII_empresa_asp.net.Models.usuario> usuarios { get; set; }
+    }
+}
